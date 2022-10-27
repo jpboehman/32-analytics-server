7a735aea005e67a2a9344c1d3fc3267ef4c9f7b9
Jackson Boehman <jpboehman@gmail.com>
2022-10-27 15:05:02 -0400

    tweaks

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -13,7 +13,7 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
     }
 
     if (user) {
-      res.status(400).send({ message: 'Failed! Username is already in use!' });
+      res.status(400).send({ message: 'Failed to create user - username already use' });
       return;
     }
 
@@ -27,7 +27,7 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
       }
 
       if (user) {
-        res.status(400).send({ message: 'Failed! Email is already in use!' });
+        res.status(400).send({ message: 'Failed to create user - username already use' });
         return;
       }
 
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ db.mongoose
     useUnifiedTopology: true
   })
   .then(() => {
-    console.log('Successfully connect to MongoDB.');
+    console.log('Successfully connected to MongoDB.');
     initial();
   })
   .catch(err => {
