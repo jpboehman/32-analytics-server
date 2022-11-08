56c0a8292ee250409c3989d9d95af2e80746c3ad
Jackson Boehman <jpboehman@gmail.com>
2022-11-08 15:22:26 -0500

    fixing route

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -2,47 +2,44 @@ const { verifySignUp } = require('../middlewares');
 const controller = require('../controllers/auth.controller');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
 
-module.exports = function (app) {
-  app.use(function (req, res, next) {
-    res.header(
-      'Access-Control-Allow-Headers',
-      'x-access-token, Origin, Content-Type, Accept'
-    );
-    next();
-  });
+module.exports = function(app) {
+	app.use(function(req, res, next) {
+		res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
+		next();
+	});
 
-  app.post(
-    '/api/auth/signup',
-    [
-      verifySignUp.checkDuplicateUsernameOrEmail,
-      verifySignUp.checkRolesExisted,
-    ],
-    controller.signup
-  );
+	app.post(
+		// LOCAL -> '/api/auth/signup',
+		'/auth/signup',
+		[ verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted ],
+		controller.signup
+	);
 
-  app.post('/api/auth/signin', controller.signin);
+	// LOCAL app.post('/api/auth/signin', controller.signin);
+	app.post('/auth/signin', controller.signin);
 
-  app.post('/api/auth/stripe-payment', async (req, res) => {
-    let { amount, id } = req.body;
-    try {
-      const payment = await stripe.paymentIntents.create({
-        amount,
-        currency: 'USD',
-        description: 'thirtytwoanalytics',
-        payment_method: id,
-        confirm: true,
-      });
-      console.log('Payment', payment);
-      res.json({
-        message: 'Payment successful',
-        success: true,
-      });
-    } catch (error) {
-      console.log('Error', error);
-      res.json({
-        message: 'Payment failed',
-        success: false,
-      });
-    }
-  });
+  // Add /api prefix for local
+	app.post('/auth/stripe-payment', async (req, res) => {
+		let { amount, id } = req.body;
+		try {
+			const payment = await stripe.paymentIntents.create({
+				amount,
+				currency: 'USD',
+				description: 'thirtytwoanalytics',
+				payment_method: id,
+				confirm: true
+			});
+			console.log('Payment', payment);
+			res.json({
+				message: 'Payment successful',
+				success: true
+			});
+		} catch (error) {
+			console.log('Error', error);
+			res.json({
+				message: 'Payment failed',
+				success: false
+			});
+		}
+	});
 };
