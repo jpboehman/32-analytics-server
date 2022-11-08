d3510a250e618141c5f9812009c7f6c7fe4eae2b
Jackson Boehman <jpboehman@gmail.com>
2022-11-08 18:51:24 -0500

    Removing comments

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.use((req, res, next) => {
-// 	res.setHeader('Access-Control-Allow-Origin', '*');
-// 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-// 	next();
-// });
 
 // parse requests of content-type - application/json
 app.use(express.json());
