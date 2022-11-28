ef5acd961519871097f9ccf26f68c53727f0350a
Jackson Boehman <jpboehman@gmail.com>
2022-11-28 16:23:32 -0500

    correcting new payments

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -3,7 +3,7 @@ const { verifySignUp } = require('../middlewares');
 const cors = require('cors');
 const controller = require('../controllers/auth.controller');
 // Test Stripe Mode:
-const stripe = require('stripe')(process.env.STRIPE_SECRET_PRODUCTION);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
 
 module.exports = function(app) {
   app.use((req, res, next) => {
