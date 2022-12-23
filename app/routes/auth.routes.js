f839c5038aa26a5c8cfa9ac74383e2c1fa54db28
Jackson Boehman <jpboehman@gmail.com>
2022-12-23 15:49:42 -0500

    tweaks

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -25,7 +25,7 @@ module.exports = function (app) {
   app.post('/api/auth/forgot-password', cors(), controller.forgotPassword);
   app.put('/api/auth/update-password-via-email', cors(), controller.updatePasswordViaEmail);
 
-  app.post('/api/create-customer-portal-session', cors(), async (req, res) => {
+  app.post('/api/auth/create-customer-portal-session', cors(), async (req, res) => {
     try {
       const { id, username, email } = req.body.params;
       const customer = await stripe.customers.search({
