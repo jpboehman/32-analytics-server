c2e5b020726098761bf25d2267c88d229caf99fc
Jackson Boehman <jpboehman@jacksons-mbp.mynetworksettings.com>
2023-06-12 20:39:17 -0400

    working

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,13 +1,13 @@
-const config = require('../config/auth.config');
-const db = require('../models');
-const nodemailer = require('nodemailer');
+const config = require("../config/auth.config");
+const db = require("../models");
+const nodemailer = require("nodemailer");
 const User = db.user;
 const Role = db.role;
 
-var jwt = require('jsonwebtoken');
-var bcrypt = require('bcryptjs');
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
 
-require('dotenv').config();
+require("dotenv").config();
 
 exports.signup = (req, res) => {
   const user = new User({
@@ -17,95 +17,62 @@ exports.signup = (req, res) => {
   });
 
   user.save((err, user) => {
+    debugger;
     if (err) {
       res.status(500).send({ message: err });
       return;
     }
 
-    if (req.body.roles) {
-      Role.find(
-        {
-          name: { $in: req.body.roles }
-        },
-        (err, roles) => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
-
-          user.roles = roles.map(role => role._id);
-          user.save(err => {
-            if (err) {
-              res.status(500).send({ message: err });
-              return;
-            }
-
-            res.send({ message: 'User was registered successfully!' });
-          });
-        }
-      );
-    } else {
-      Role.findOne({ name: 'user' }, (err, role) => {
-        if (err) {
-          res.status(500).send({ message: err });
-          return;
-        }
-
-        user.roles = [role._id];
-        user.save(err => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
-
-          res.send({ message: 'User was registered successfully!' });
-        });
-      });
-    }
+    console.log(`Making it here? ${JSON.stringify(user)}`);
   });
 };
 
 exports.signin = (req, res) => {
   User.findOne({
-    username: req.body.username
+    email: req.body.email,
   })
-    .populate('roles', '-__v')
+    .populate("roles", "-__v")
     .exec((err, user) => {
       if (err) {
         res.status(500).send({ message: err });
         return;
       }
+      console.log(`user is: ${user}`);
 
       if (!user) {
-        return res.status(404).send({ message: 'User Not found.' });
+        return res.status(404).send({ message: "User Not found." });
       }
 
-      
       const passwordIsValid = bcrypt.compareSync(
         req.body.password,
         user.password
       );
 
       if (!passwordIsValid) {
-        return res.status(401).send({ accessToken: null, message: 'Invalid password' })
-      } 
+        return res
+          .status(401)
+          .send({ accessToken: null, message: "Invalid password" });
+      }
 
       // This server-side code creates the token once a user is validated, and is returned in the response
       // Every subsequent request from the client will contain that JWT to ensure the party is authenticated
       const token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400
-      })
+        expiresIn: 86400,
+      });
       var authorities = [];
 
       for (let i = 0; i < user.roles.length; i++) {
-        authorities.push('ROLE_' + user.roles[i].name.toUpperCase());
+        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
       }
+
+      console.log(`user at line 87: ${JSON.stringify(user)}`);
+
       res.status(200).send({
         id: user._id,
         username: user.username,
         email: user.email,
         roles: authorities,
-        accessToken: token
+        accessToken: token,
       });
     });
 };
@@ -113,17 +80,17 @@ exports.signin = (req, res) => {
 // Responsible for locating the user, generating a token and emailing it to the user to ensure they provide it when they re-enter email
 exports.forgotPassword = (req, res) => {
   User.findOne({
-    email: req.body.email
+    email: req.body.email,
   })
-    .populate('roles', '-__v')
+    .populate("roles", "-__v")
     .exec(async (err, user) => {
       if (err) {
-        console.log(err)
+        console.log(err);
         res.status(500).send({ message: err });
         return;
       }
       if (!user) {
-        return res.status(404).send({ message: 'User Not found.' });
+        return res.status(404).send({ message: "User Not found." });
       }
 
       const token = jwt.sign({ id: user.id }, config.secret, {
@@ -132,64 +99,66 @@ exports.forgotPassword = (req, res) => {
       // Add the resetPasswordToken to the database for the user
       await user.update({
         resetPasswordToken: token,
-        resetPasswordExpires: Date.now() + 3600000
+        resetPasswordExpires: Date.now() + 3600000,
       });
 
       const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: "gmail",
         auth: {
           user: `${process.env.EMAIL_ADDRESS}`,
           pass: `${process.env.EMAIL_PASSWORD}`,
         },
-      })
+      });
 
       const mailOptions = {
         from: `${process.env.EMAIL_ADDRESS}`,
         to: `${user.email}`,
-        subject: 'Link to Reset Password for 32Analytics',
-        text: `You are receiving this because you (or someone else) have requested the reset of the password for your account. \n\n`
-          + 'Please click on the following link, or paste this into your browser to complete the process within one hour of receiving this email:\n\n'
-          + `https://thirtytwoanalytics.com/reset-password-submit/${token}\n\n`
-          + 'If you did not request this, please ignore this email, as your password will remain unchanged.\n',
+        subject: "Link to Reset Password for 32Analytics",
+        text:
+          `You are receiving this because you (or someone else) have requested the reset of the password for your account. \n\n` +
+          "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving this email:\n\n" +
+          `https://thirtytwoanalytics.com/reset-password-submit/${token}\n\n` +
+          "If you did not request this, please ignore this email, as your password will remain unchanged.\n",
       };
 
       transporter.sendMail(mailOptions, (err, response) => {
         if (err) {
-          console.error('the following error occurred: ', err);
+          console.error("the following error occurred: ", err);
         } else {
-          console.log('response: ', response);
-          res.status(200).send({ message: 'Reset email sent.' })
+          console.log("response: ", response);
+          res.status(200).send({ message: "Reset email sent." });
         }
       });
     });
-}
+};
 
 exports.reset = (req, res) => {
-  // WORKING NOW!
   User.findOne({
     resetPasswordToken: req.query.resetPasswordToken,
     resetPasswordExpires: {
       $gt: Date.now(),
     },
   })
-    .populate('roles', '-__v')
+    .populate("roles", "-__v")
     .exec((err, user) => {
       if (err) {
-        console.error('Password reset link is invalid or expired.')
-        res.status(403).send({ message: 'Password reset link is invalid or has expired.' });
+        console.error("Password reset link is invalid or expired.");
+        res
+          .status(403)
+          .send({ message: "Password reset link is invalid or has expired." });
         return;
       }
       if (!user) {
-        return res.status(404).send({ message: 'User not found.' });
+        return res.status(404).send({ message: "User not found." });
       } else {
         // Ensuring that the user has a valid reset token in order for them to access to the reset component
         res.status(200).send({
           username: user.username,
-          message: 'Password reset link is VALID.'
-        })
+          message: "Password reset link is VALID.",
+        });
       }
     });
-}
+};
 
 const BCRYPT_SALT_ROUNDS = 8;
 exports.updatePasswordViaEmail = (req, res) => {
@@ -199,31 +168,38 @@ exports.updatePasswordViaEmail = (req, res) => {
     resetPasswordToken: req.body.resetPasswordToken,
     resetPasswordExpires: {
       $gt: Date.now(),
-    }
+    },
   })
-    .populate('roles', '-__v')
+    .populate("roles", "-__v")
     .exec(async (err, user) => {
       if (err) {
-        res.status(500).send({ message: 'Password reset link invalid or has expired' });
+        res
+          .status(500)
+          .send({ message: "Password reset link invalid or has expired" });
         return;
       }
       if (err) {
-        res.status(500).send({ message: 'Password reset link is invalid or expired' });
+        res
+          .status(500)
+          .send({ message: "Password reset link is invalid or expired" });
       }
       if (!user) {
-        return res.status(404).send({ message: 'User Not found.' });
+        return res.status(404).send({ message: "User Not found." });
       } else {
-        console.log('User who has requested password change has been found');
-        const hashedPassword = await bcrypt.hashSync(req.body.password, BCRYPT_SALT_ROUNDS)
+        console.log("User who has requested password change has been found");
+        const hashedPassword = await bcrypt.hashSync(
+          req.body.password,
+          BCRYPT_SALT_ROUNDS
+        );
         await user.update({
           password: hashedPassword,
           resetPasswordToken: null,
           resetPasswordExpires: null,
         });
-        console.log('Password updated successfully.')
+        console.log("Password updated successfully.");
         res.status(200).send({
-          message: 'Password reset submission was submitted successfully.'
-        })
+          message: "Password reset submission was submitted successfully.",
+        });
       }
     });
-}
+};
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,84 +1,87 @@
 /* eslint-disable strict */
-const { verifySignUp } = require('../middlewares');
-const cors = require('cors');
-const controller = require('../controllers/auth.controller');
+const { verifySignUp } = require("../middlewares");
+const cors = require("cors");
+const controller = require("../controllers/auth.controller");
 // Test Stripe Mode:
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
-const stripe = require('stripe')(process.env.STRIPE_SECRET_PRODUCTION);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_PRODUCTION);
 
 module.exports = function (app) {
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, Content-Type, Accept');
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
     next();
   });
 
   app.post(
-    '/api/auth/signup',
-    [verifySignUp.checkDuplicateUsernameOrEmail, verifySignUp.checkRolesExisted],
+    "/api/auth/signup",
+    [
+      verifySignUp.checkDuplicateUsernameOrEmail,
+      verifySignUp.checkRolesExisted,
+    ],
+    cors(),
     controller.signup
   );
 
-  app.post('/api/auth/signin', cors(), controller.signin);
+  app.post("/api/auth/signin", cors(), controller.signin);
 
   // Forgot password route:
-  app.get('/api/auth/reset', cors(), controller.reset);
-  app.post('/api/auth/forgot-password', cors(), controller.forgotPassword);
-  app.put('/api/auth/update-password-via-email', cors(), controller.updatePasswordViaEmail);
+  app.get("/api/auth/reset", cors(), controller.reset);
+  app.post("/api/auth/forgot-password", cors(), controller.forgotPassword);
+  app.put(
+    "/api/auth/update-password-via-email",
+    cors(),
+    controller.updatePasswordViaEmail
+  );
 
-  app.post('/api/auth/create-customer-portal-session', async (req, res) => {
+  app.post("/api/auth/create-customer-portal-session", async (req, res) => {
     try {
       const { email } = req.body.params;
       const customer = await stripe.customers.search({
         query: `email:\'${email}\'`,
       });
-      
+
       const custId = customer.data[0].id;
       if (!custId) {
-        res.status(404).send({ message: 'User not found' });
+        res.status(404).send({ message: "User not found" });
       }
 
       const session = await stripe.billingPortal.sessions.create({
         customer: custId,
-        return_url: 'https://thirtytwoanalytics.com',
+        return_url: "https://thirtytwoanalytics.com",
       });
 
       res.status(200).send({ redirectUrl: session.url });
     } catch (error) {
-      res.status(404).send({ message: `Unable to locate the customer's email address, please try again.` })
+      res.status(404).send({
+        message: `Unable to locate the customer's email address, please try again.`,
+      });
     }
   });
 
-  app.post('/api/auth/stripe-payment', cors(), async (req, res) => {
+  app.post("/api/auth/stripe-payment", cors(), async (req, res) => {
     const { amount, id } = req.body;
     try {
       const payment = await stripe.paymentIntents.create({
         amount,
-        currency: 'USD',
-        description: 'thirtytwoanalytics',
+        currency: "USD",
+        description: "thirtytwoanalytics",
         payment_method: id,
-        confirm: true
+        confirm: true,
       });
-      console.log('Payment', payment);
+      console.log("Payment", payment);
       res.json({
-        message: 'Payment successful',
-        success: true
+        message: "Payment successful",
+        success: true,
       });
     } catch (error) {
-      // Was erroring here
-      console.log('Error', error);
+      console.log("Error", error);
       res.json({
-        message: 'Payment failed',
-        success: false
+        message: "Payment failed",
+        success: false,
       });
     }
   });
 };
-
-/*
-Everyone in it together
-Walk through conflict in a way that's connecting
-Feedback for framework, making that nourishing, especially for the tough stuff
-Mention the book the Advantage - organizational health
-Reflective listenting, clarifying questions
-Emphasizing empathy and connection
-*/
